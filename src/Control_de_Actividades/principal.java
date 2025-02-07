a4bc67dec5abcc16c1ecae27b2c346f7e2d1b067
gustavex22 <gustavex618@gmail.com>
2025-02-07 13:09:58 -0500

    Empeze un nuevo proyecto un juntos con las actividades a hacer

---
+++ src/Control_de_Actividades/principal.java
@@ -0,0 +1,17 @@
+package Control_de_Actividades;
+
+public class principal {
+    /*
+     * Programa de control de Actividades
+     * funciones:
+     * Menu
+     * Añadir Actividades
+     * Ver Actividades
+     * Eliminar actividades
+     * Marcas como Completado las actividades
+     */
+
+    /* contruccion
+     *  [Status] Nombre de la Actividad : Descripcion
+     */
+}

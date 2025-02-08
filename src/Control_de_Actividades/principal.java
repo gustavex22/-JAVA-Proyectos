777ab7a989d1bee64fe614d7d43f3921675844c3
gustavex22 <gustavex618@gmail.com>
2025-02-08 13:00:53 -0500

    agregado del Menu

--- src/Control_de_Actividades/principal.java
+++ src/Control_de_Actividades/principal.java
@@ -1,10 +1,12 @@
 package Control_de_Actividades;
 
+import java.util.Scanner;
+
 public class principal {
     /*
      * Programa de control de Actividades
      * funciones:
-     * Menu
+     * Menu [Listo]
      * Añadir Actividades
      * Ver Actividades
      * Eliminar actividades
@@ -14,4 +16,25 @@ public class principal {
     /* contruccion
      *  [Status] Nombre de la Actividad : Descripcion
      */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Option=0;
+
+
+        System.out.println("""
+
+                __Control de Actividades__
+
+                1.Ver Actividades
+                2.Añadir Actividades
+                3.Marcar como completado una ACT.
+                4.Eliminar Actividad
+                """);
+
+//? Entrada de datos:Simple codigo para guardar la opcion elegida en una Variable
+
+        System.out.print("Introducir Opcion:");
+        Option = sc.nextInt();
+    }
 }

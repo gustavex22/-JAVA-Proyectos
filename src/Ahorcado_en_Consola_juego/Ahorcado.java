5a711d63c6689261c40ac5136ee460e190186913
gustavex22 <gustavex618@gmail.com>
2025-02-24 22:48:11 -0500

    [Nuevo Proyecto]Prepare el proyecto para el juego del ahorcado junto con las actividades a realizar

---
+++ src/Ahorcado_en_Consola_juego/Ahorcado.java
@@ -0,0 +1,14 @@
+package Ahorcado_en_Consola_juego;
+
+public class Ahorcado {
+    public static void main(String[] args) {
+        /*
+         * Simular el juego del ahorcado en java
+         * Actividades:
+         * Introducir una palabra.
+         * Introducir una Letra
+         * Comparar las la letra con las letra de las palabras
+         */
+    }
+
+}

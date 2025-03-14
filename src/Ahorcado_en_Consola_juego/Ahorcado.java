1a610d3cfee9e294e3518c81fe481a6e79e0c6b3
gustavex22 <gustavex618@gmail.com>
2025-03-14 14:02:49 -0500

    [Avanze] Recurri a usar 2sentencias while concatenadas para el menu y dibujado , ademas avanze un poco el codigo para el dibujado del muñeco

--- src/Ahorcado_en_Consola_juego/Ahorcado.java
+++ src/Ahorcado_en_Consola_juego/Ahorcado.java
@@ -1,8 +1,9 @@
 package Ahorcado_en_Consola_juego;
 
+import java.util.Scanner;
+
 public class Ahorcado {
-    public static void main(String[] args) {
-        /*
+    /*
          * Simular el juego del ahorcado en java
          * Actividades:
          * Importar archivo txt como base de datos
@@ -17,6 +18,74 @@ public class Ahorcado {
          * procedera a comparar la letra que introduzcamos con la variable
          * dibujara en consola como el ahorcado va actualizando su estado segun no acertemos en la palabra
          */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Palabra,Genero;
+        int Opcion_User=0;
+
+        //? Controla el bucle while de cada seccion del juego
+        boolean Menu = true,Dibujado=true;
+
+        //*Menu
+        while (Menu) {
+
+            System.out.println("#### Ahorcado The game#####");
+            System.out.println("1. Jugar");
+            System.out.println("2. Salir ");
+            
+            System.out.print("Introduce la Opcion:");
+            Opcion_User = sc.nextInt();
+
+            //?Opciones del menu
+            switch (Opcion_User) {
+
+                case 1:
+                System.out.println("Iniciando Juego...");
+                Menu = true;
+                break;
+
+                case 2:
+                System.out.println("Saliendo..");
+                Menu = false;
+                    break;
+            }
+
+            while(Dibujado){
+                int Estatus=0;
+                String Ahorcado = "";
+
+                switch (Estatus) {
+                    case 0:
+                        Ahorcado = """
+                                _______________
+                                ||           |
+                                ||           |
+                                ||           O
+                                ||          /|\
+                                
+                                ||         / | \
+                                
+                                ||          / \
+                                
+                                ||         /   \
+                                
+                                ||
+                                """;
+                        
+                        break;
+                
+                    default:
+                        break;
+                }
+                
+                System.out.println(Ahorcado+"\n");
+                Dibujado = false;
+
+
+            }
+
+            
+        }
     }
 
 }
